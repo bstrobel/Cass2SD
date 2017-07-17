5e4827f1540dbb09a0ef3ec1cb0de18f17cee007
bstrobel <strobelb69@gmail.com>
2017-07-17 18:10:25 +0200

    Recording works for the first time!

--- main/kc_cass_recv_file.c
+++ main/kc_cass_recv_file.c
@@ -11,6 +11,7 @@
 #include <avr/pgmspace.h>
 #include <string.h>
 #include <util/delay.h>
+#include <util/atomic.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include "kc_cass_common.h"
@@ -19,18 +20,49 @@
 #include "../ff_avr/xitoa.h"
 #include "../lcd/lcd.h"
 
-#define RECV_TIMER_TIMEOUT_CNT 16000
-#define RECV_TIMER_ZERO_THRESHOLD 3000
-#define RECV_TIMER_ONE_THRESHOLD 6000
-#define MIN_ONE_BITS_IN_VORTON 20
+// Clock_Timer1(Prescaler=8MHz/8=1MHz -> 1 tick = 1탎
+// f_One=1000Hz -> 1ms/2 => 500탎
+// f_Zero=2000Hz -> 500탎/2 => 250탎
+// f_Space=500Hz -> 2ms/2 => 1000탎
+// 1ms => 1000 ticks
+#define RECV_TIMER_TIMEOUT_CNT 64000U			// 64ms
+#define RECV_TIMER_ZERO_UPPER_THRESHOLD 375U	// 0.375ms
+#define RECV_TIMER_ONE_UPPER_THRESHOLD 750U		// 0.75ms
+
+#define IS_SPACE(ctr) (ctr > RECV_TIMER_ONE_UPPER_THRESHOLD)
+#define IS_ONE(ctr) (ctr > RECV_TIMER_ZERO_UPPER_THRESHOLD && ctr <= RECV_TIMER_ONE_UPPER_THRESHOLD)
+#define IS_ZERO(ctr) (ctr <= RECV_TIMER_ZERO_UPPER_THRESHOLD)
+
+// Timer config
+// TIMSK1 = _BV(OCIE1A); // allow interrupts for OCR1A match
+// TIMSK1 = _BV(TOIE1); // allow TIMER1 Overflow
+// TCCR1B = _BV(WGM12) || _BV(CS11); // Enable timer in CTC-Mode (WGM12), pre-scaler = 8 (CS11), 1MHz timer clock
+// TCCR1B = _BV(CS11); // Enable timer in Normal-Mode, TOP=MAX, pre-scaler = 8 (CS11), 1MHz timer clock
+#define START_TIMER1 {\
+	TCCR1B = 0; \
+	TCNT1 = 0; \
+	TIMSK1 = _BV(OCIE1A); \
+	TCCR1B = _BV(WGM12) | _BV(CS11); \
+}
+
+
+// TIMSK1 = 0; // disallow all interrupts for timer1
+// TCCR1B = 0; // stop timer
+// TIFR1 = 0; // clear all interrupt flags
+#define STOP_TIMER1 { \
+	TIMSK1 = 0; \
+	TCCR1B = 0; \
+	TIFR1 = 0; \
+}
+
+#define MIN_ONE_BITS_IN_VORTON 9U
+#define IS_VORTON(ctr) (ctr >= MIN_ONE_BITS_IN_VORTON)
+
 #define DISK_BLOCK_SIZE (DATA_BUF_SIZE - 1)
-#define IS_SPACE(ctr) (ctr > RECV_TIMER_ONE_THRESHOLD)
-#define IS_ONE(ctr) (ctr > RECV_TIMER_ZERO_THRESHOLD && ctr <= RECV_TIMER_ONE_THRESHOLD)
-#define IS_ZERO(ctr) (ctr <= RECV_TIMER_ZERO_THRESHOLD)
 #define MAX_BUF_IDX (DATA_BUF_SIZE - 1)
 
 typedef enum {
-	RECV_VORTON_DETECTED, 
+	RECV_VORTON_DETECTED,
 	RECV_BYTE_READY, 
 	RECV_BIT_TIMEOUT, 
 	RECV_HANDLER_ACK, 
@@ -38,48 +70,32 @@ typedef enum {
 } recv_state_enum;
 volatile recv_state_enum recv_state = RECV_HANDLER_ACK;
 
-volatile bool is_in_recv_state = false;
+volatile bool is_receiving = false;
 volatile uint8_t buf_idx = 0;
 char file_name_on_disk[LEN_DATEINAME + 1 + LEN_DATEITYP + 1]; // contains 8byte name + 1 dot + 3byte ext + 0x00byte
 char file_type[LEN_DATEITYP + 1]; // 3byte + 0x00byte
 const char msg_info_saved_str[] PROGMEM = "INFO: FILE SAVED";
 
-volatile bool is_begin_measure = true; // if true ISR(INT0_vect) starts a new time measurement
+volatile bool is_time_measure_running = false; // if true ISR(INT0_vect) starts a new time measurement
 volatile uint8_t recv_byte = 0;
 volatile uint8_t block_cntr = 0;
 volatile uint16_t vorton_cntr = 0;
+volatile uint16_t cntr_val = 0;
 
-static void config_and_start_timer_recv_file() {
-	OCR1A = RECV_TIMER_TIMEOUT_CNT; // set TOP of counter to be reached after 2msec
-	TCNT1 = 0; // reset counter 
-	TIFR1 = 0; // clear interrupt flags
-	TIMSK1 = _BV(OCIE1A); // allow interrupts for OCR1A match
-	TCCR1A = 0;
-	TCCR1B = _BV(WGM12) || _BV(CS10); // Enable timer in CTC-Mode, pre-scaler = 1, 8MHz timer clock
-}
-
-static void stop_timer_recv_file() {
-	TCCR1B = 0;
-	TCCR1A = 0;
-	TIMSK1 = 0;
-	TIFR1 = 0;
-}
-
-static void reset_state_after_error_or_end() {
-	is_in_recv_state = false;
+static void reset_recv_state() {
+	STOP_TIMER1;
+	is_receiving = false;
 	block_cntr = 0;
 	buf_idx = 0;
 	file_name_on_disk[0] = 0;
-	#ifdef DEBUG_RECV_TIMER
-	MONITOR_RECV_PORT &= ~_BV(MONITOR_RECV_BIT);
-	#endif
 }
 
 // should only be fired when timer is not reset before,
 // meaning this is a timeout (doesn't need to be an error,
 // can be at end of file)
+//ISR(TIMER1_OVF_vect) {
 ISR(TIMER1_COMPA_vect) {
-	stop_timer_recv_file();
+	is_time_measure_running = false;
 	if (recv_state == RECV_HANDLER_ACK) {
 		recv_state = RECV_BIT_TIMEOUT;
 	}
@@ -90,25 +106,19 @@ ISR(TIMER1_COMPA_vect) {
 
 // is fired at each change in logical level of pin
 ISR(INT0_vect) {
-	if (is_begin_measure) {
-		// initialize and start timer
-		config_and_start_timer_recv_file();
-		is_begin_measure = false;
-	} else {
-		// stop timer and shift in bit
-		stop_timer_recv_file();
-		uint16_t cntr_val = TCNT1;
-		
+	cntr_val = TCNT1;
+	#ifdef DEBUG_RECV_TIMER
+	MONITOR_RECV_PIN1_HIGH;
+	#endif
+	if (is_time_measure_running) {
+		START_TIMER1; // need to start it to catch the last timeout
 		if (recv_state != RECV_HANDLER_ACK) {
 			recv_state = RECV_OVERRUN_OCCURED;
 		}
 		else {
 			if (IS_SPACE(cntr_val)) {
-				if (vorton_cntr > MIN_ONE_BITS_IN_VORTON) {
+				if (IS_VORTON(vorton_cntr)) {
 					recv_state = RECV_VORTON_DETECTED;
-					#ifdef DEBUG_RECV_TIMER
-					MONITOR_RECV_PORT |= _BV(MONITOR_RECV_BIT);
-					#endif
 				}
 				else {
 					recv_state = RECV_BYTE_READY;
@@ -119,17 +129,26 @@ ISR(INT0_vect) {
 				recv_byte >>= 1;
 				if (IS_ONE(cntr_val)) {
 					vorton_cntr++;
-					recv_byte += 0b10000000;
-					} else if (IS_ZERO(cntr_val)) {
+					recv_byte |= 0x80;
+				}
+				else { // if (IS_ZERO(cntr_val))
 					vorton_cntr=0;
 				}
 			}
 		}
-		is_begin_measure = true;
+		is_time_measure_running = false;
 	}
+	else {
+		// initialize and start timer
+		START_TIMER1;
+		is_time_measure_running = true;
+	}
+	#ifdef DEBUG_RECV_TIMER
+	MONITOR_RECV_PIN1_LOW;
+	#endif
 }
 
-static bool get_filename_from_fcb() {
+static void get_filename_from_fcb() {
 	char* dateiname;
 	char* dateityp;
 	if (check_is_basic_fcb()) {
@@ -165,19 +184,19 @@ static bool get_filename_from_fcb() {
 		file_name_on_disk[i] = dateiname[i];
 	}
 	if (i == 0) {
-		return false; // zero length filename -> something is wrong
+		strlcpy_P(file_name_on_disk,PSTR(DEFAULT_FILE_NAME),9);
+		i=8;
 	}
-	else {
-		file_name_on_disk[i] = '.';
-		strlcpy_P(file_name_on_disk+i+1,PSTR(FILE_EXT),4);
-		return true;
-	}
+	file_name_on_disk[i] = '.';
+	strlcpy_P(file_name_on_disk+i+1,PSTR(FILE_EXT),4);
 }
 
 void kc_cass_recv_file_init() {
 	#ifdef DEBUG_RECV_TIMER
-	MONITOR_RECV_DDR |= _BV(MONITOR_RECV_BIT);
-	MONITOR_RECV_PORT &= ~_BV(MONITOR_RECV_BIT);
+	MONITOR_RECV_DDR |= (_BV(MONITOR_RECV_BIT1) | _BV(MONITOR_RECV_BIT2) | _BV(MONITOR_RECV_BIT3));
+	MONITOR_RECV_PIN1_LOW;
+	MONITOR_RECV_PIN2_LOW;
+	MONITOR_RECV_PIN3_LOW;
 	#endif
 	
 	CASS_IN_DDR &= ~_BV(CASS_IN_PIN); // Configure input pin as INPUT
@@ -185,7 +204,9 @@ void kc_cass_recv_file_init() {
 	EIFR &= _BV(INT0); // clear int flag reg
 	EIMSK |= _BV(INT0); // enable int INT0
 	EICRA = _BV(ISC00); // any change in logical level on pin will generate int
-	reset_state_after_error_or_end();
+	OCR1A = RECV_TIMER_TIMEOUT_CNT; // set TOP of counter
+	TCCR1A = 0; // COM1[A,B][1,0]=0 => Compare Output Mode=normal; WGM[10,11]=0 => Normal or CTC mode
+	reset_recv_state();
 }
 
 void kc_cass_recv_file_disable() {
@@ -197,12 +218,20 @@ void kc_cass_recv_file_disable() {
 /************************************************************************/
 /* To be called from the main while loop                                */
 /************************************************************************/
-void kc_cass_handle_recv_file() {
-	switch (recv_state) {
+bool kc_cass_handle_recv_file() {
+	recv_state_enum _recv_state;
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		_recv_state = recv_state;
+		recv_state = RECV_HANDLER_ACK;
+	}
+	#ifdef DEBUG_RECV_TIMER
+	MONITOR_RECV_PIN3_HIGH;
+	#endif
+	switch (_recv_state) {
 		case RECV_OVERRUN_OCCURED: {
-			reset_state_after_error_or_end();
-			f_close(&fhdl);
-			if (is_in_recv_state) {
+			if (is_receiving) {
+				reset_recv_state();
+				f_close(&fhdl);
 				disp_msg_p(msg_error_str,PSTR("OVERRUN!")); // we are only interested in overruns during receive
 			}
 			break;
@@ -210,8 +239,8 @@ void kc_cass_handle_recv_file() {
 		case RECV_VORTON_DETECTED: {
 			memset(buf,0x0,DATA_BUF_SIZE);
 			buf_idx = 0;
-			if (!is_in_recv_state) {
-				is_in_recv_state = true;
+			if (!is_receiving) {
+				is_receiving = true;
 				block_cntr = 0;
 				display_recvinfo(NULL,0);
 			}
@@ -219,17 +248,17 @@ void kc_cass_handle_recv_file() {
 		}
 		case RECV_BYTE_READY: {
 			#ifdef DEBUG_RECV_TIMER
-			MONITOR_RECV_PORT &= ~_BV(MONITOR_RECV_BIT);
+			MONITOR_RECV_PIN2_TOGGLE;
 			#endif
-			if (is_in_recv_state) {
-				buf_idx++;
+			if (is_receiving) {
 				if (buf_idx < MAX_BUF_IDX) {
 					buf[buf_idx] = recv_byte;
-					break;
+					buf_idx++;
+					recv_byte = 0;
 				}
-				if (buf_idx == MAX_BUF_IDX) { // block completely received
+				else if (buf_idx == MAX_BUF_IDX) { // block completely received
 					if (recv_byte != calculate_checksum()) {
-						reset_state_after_error_or_end();
+						reset_recv_state();
 						f_close(&fhdl);
 						lcd_clrscr();
 						xprintf(PSTR("WRONG CHECKSUM"));
@@ -241,30 +270,25 @@ void kc_cass_handle_recv_file() {
 
 					if (block_cntr == 0) { // first block
 						f_close(&fhdl); // just to be sure
-						if (!get_filename_from_fcb()) {
-							reset_state_after_error_or_end();
-							disp_msg_p(msg_error_str,PSTR("FILENAME=\"\""));
+						get_filename_from_fcb();
+						fr = f_open(&fhdl, file_name_on_disk, FA_WRITE | FA_CREATE_ALWAYS);
+						if (fr != FR_OK) {
+							reset_recv_state();
+							f_close(&fhdl);
+							disp_fr_err(fr);
+							disp_msg_p(msg_error_str,PSTR("OPEN FOR WRITE"));
 							break;
 						}
-						else {
-							fr = f_open(&fhdl, file_name_on_disk, FA_WRITE | FA_CREATE_ALWAYS);
-							if (fr != FR_OK) {
-								reset_state_after_error_or_end();
-								f_close(&fhdl);
-								disp_fr_err(fr);
-								disp_msg_p(msg_error_str,PSTR("OPEN FOR WRITE"));
-								break;
-							}
-							else {
-								display_recvinfo(file_name_on_disk,buf[0]);
-							}
-						}
+						display_recvinfo(file_name_on_disk,buf[0]);
+					}
+					else {
+						display_upd_recvinfo(buf[0]);
 					}
 
 					UINT bytes_written = 0;
 					fr = f_write(&fhdl, buf, DISK_BLOCK_SIZE, &bytes_written);
 					if (fr != FR_OK || bytes_written != DISK_BLOCK_SIZE) {
-						reset_state_after_error_or_end();
+						reset_recv_state();
 						f_close(&fhdl);
 						if (fr != FR_OK) {
 							disp_fr_err(fr);
@@ -274,23 +298,24 @@ void kc_cass_handle_recv_file() {
 					}
 					block_cntr++;
 					if (block_cntr == 0) {
-						reset_state_after_error_or_end();
+						reset_recv_state();
 						f_close(&fhdl);
 						disp_msg_p(msg_info_saved_str,PSTR("MAX BLOCKS!"));
+						break;
 					}
-					break;
 				}
-				if (buf_idx > MAX_BUF_IDX) {
-					reset_state_after_error_or_end();
+				else { //  if (buf_idx > MAX_BUF_IDX)
+					reset_recv_state();
 					f_close(&fhdl);
 					disp_msg_p(msg_error_str,PSTR("BLKSIZE EXCEEDED"));
+					break;
 				}
 			}
 			break;
 		}
 		case RECV_BIT_TIMEOUT: {
-			if (is_in_recv_state) {
-				reset_state_after_error_or_end();
+			if (is_receiving) {
+				reset_recv_state();
 				f_close(&fhdl);
 				disp_msg_p(msg_info_saved_str,PSTR(""));
 			}
@@ -301,6 +326,8 @@ void kc_cass_handle_recv_file() {
 			break;
 		}
 	}
-	recv_byte = 0;
-	recv_state = RECV_HANDLER_ACK;
+	#ifdef DEBUG_RECV_TIMER
+	MONITOR_RECV_PIN3_LOW;
+	#endif
+	return is_receiving;
 }
